4a4f24110893637ec627ac04dacf14f38b03bf92
Carey Johnson <johnson.careyj@gmail.com>
2018-09-13 11:10:41 -0700

    added the app component to the dom

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component. This component should produce
 //some HTML
+
+const App = () =>  {
+  return <div>Hi!</div>;
+};
+
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
+// take this component's generated HTML and put it in on the page (in the DOM)
+
