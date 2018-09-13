1443ae1fdbe34a0da7189b73bbddeef9e32337ce
Carey Johnson <johnson.careyj@gmail.com>
2018-09-13 11:25:13 -0700

    added new js files and API KEY

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDlqKtm6JTx1zpSKSa9ijuEcbnMuse_n2k';
+
 // Create a new component. This component should produce
 //some HTML
 
@@ -9,8 +11,8 @@ const App = () =>  {
 };
 
 
+// take this component's generated HTML and put it in on the page (in the DOM)
 const root = document.getElementById('root');
 
 ReactDOM.render(<App />, root);
-// take this component's generated HTML and put it in on the page (in the DOM)
 
