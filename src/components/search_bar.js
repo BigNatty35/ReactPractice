ffb6a66a703d822d9e52bc03e3be5e5fbbfd2060
Carey Johnson <johnson.careyj@gmail.com>
2018-09-13 19:49:15 -0700

    functionality to the search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
-  render() {
-    return <input/>;
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
   }
+  render() {
+    return (
+      <div className="search-bar">
+        <input 
+          value={this.state.term}
+          onChange={e => this.setState({term: e.target.value})}/>
+      </div> 
+    );
+  }
+
 }
 
 
